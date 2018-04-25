25bd54296aa8382e48c745fa15590aeb8cf628c7
david <dlsloan@ualberta.ca>
2018-04-24 23:42:58 -0600

    Added get_rect and draw_mirror to sprite class

--- Sprite.py
+++ Sprite.py
@@ -4,8 +4,18 @@ class Sprite :
     def __init__(self, image):
         self.image = image
 
-    def draw(self, screen, position, direction):
-        rotImg = pygame.transform.rotate(self.image, direction)
+    def draw(self, screen, position, rotation):
+        rotImg = pygame.transform.rotate(self.image, rotation)
         rotRect = rotImg.get_rect()
         rotRect.center = position
         screen.blit(rotImg, rotRect)
+
+    def draw_mirror(self, screen, position, rotation):
+        rotImg = pygame.transform.flip(self.image, True, False)
+        rotImg = pygame.transform.rotate(rotImg, rotation)
+        rotRect = rotImg.get_rect()
+        rotRect.center = position
+        screen.blit(rotImg, rotRect)
+
+    def get_rect(self):
+        return self.image.get_rect()
--- main.py
+++ main.py
@@ -64,20 +64,29 @@ def main():
     running = True
     duck_x = 0
     duck_y = 0
+    duck_dir = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
                 duck_x, duck_y = move_down(game, assets, duck_x, duck_y)
+                duck_dir = 90
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
                 duck_x, duck_y = move_up(game, assets, duck_x, duck_y)
+                duck_dir = 270
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
                 duck_x, duck_y = move_left(game, assets, duck_x, duck_y)
+                duck_dir = 0
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
                 duck_x, duck_y = move_right(game, assets, duck_x, duck_y)
+                duck_dir = 180
 
-        game.screen.blit(assets.duck2_sprite, (duck_x, duck_y))
+        duck_rect = assets.duck2_sprite.get_rect()
+        if (duck_dir == 180):
+            assets.duck2_sprite.draw_mirror(game.screen, (duck_x + duck_rect.width / 2, duck_y + duck_rect.height / 2), 0)
+        else:
+            assets.duck2_sprite.draw(game.screen, (duck_x + duck_rect.width / 2, duck_y + duck_rect.height / 2), duck_dir)
         game.update_display()
 
 
